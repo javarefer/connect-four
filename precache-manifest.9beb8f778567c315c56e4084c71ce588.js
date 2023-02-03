670fad6750d0a172502dbff8252b2477b3effd3b
Rahul <rahulgs@gmail.com>
2023-02-03 15:15:54 -0500

    Updates

---
+++ precache-manifest.9beb8f778567c315c56e4084c71ce588.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5c2c49fdb4f0cff247f55b792a03afb",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "8e840aaf336b73e3d300",
+    "url": "/connect-four/static/css/main.95e840f2.chunk.css"
+  },
+  {
+    "revision": "f55269101e74d66d011f",
+    "url": "/connect-four/static/js/2.99100f30.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/connect-four/static/js/2.99100f30.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e840aaf336b73e3d300",
+    "url": "/connect-four/static/js/main.a91e67a1.chunk.js"
+  },
+  {
+    "revision": "6db8dadaa2897c54e0a6",
+    "url": "/connect-four/static/js/runtime-main.99c2f79d.js"
+  },
+  {
+    "revision": "e45d1e071a02cdb9084b5da8e922cdcb",
+    "url": "/connect-four/static/media/draw.e45d1e07.wav"
+  },
+  {
+    "revision": "590f793358d1556efab37f1209d30f05",
+    "url": "/connect-four/static/media/play1.590f7933.wav"
+  },
+  {
+    "revision": "7e864110aab96c9d9bbf1e33560c5d26",
+    "url": "/connect-four/static/media/play2.7e864110.wav"
+  },
+  {
+    "revision": "1746cdf253a897eda73e7961e6fdd4e9",
+    "url": "/connect-four/static/media/start.1746cdf2.wav"
+  },
+  {
+    "revision": "bac6a98169e77e9c56d78b9f9a18d227",
+    "url": "/connect-four/static/media/win1.bac6a981.wav"
+  },
+  {
+    "revision": "7f67be7fa1e49794612b0385a950713e",
+    "url": "/connect-four/static/media/win2.7f67be7f.wav"
+  },
+  {
+    "revision": "297d878fb2ecadae91339f2188ad0089",
+    "url": "/connect-four/static/media/win3.297d878f.wav"
+  },
+  {
+    "revision": "9c8155cb9784b4d37505d7d6d5e7581e",
+    "url": "/connect-four/static/media/win4.9c8155cb.wav"
+  },
+  {
+    "revision": "2f4f82527123a01f5afe938c89d5651c",
+    "url": "/connect-four/static/media/win5.2f4f8252.wav"
+  },
+  {
+    "revision": "af771d688a11da3cc480d348be9f3b0c",
+    "url": "/connect-four/static/media/win6.af771d68.wav"
+  },
+  {
+    "revision": "52c493342bae547e593b9c722efc43ce",
+    "url": "/connect-four/static/media/win7.52c49334.mp3"
+  },
+  {
+    "revision": "c24f65f14b8aa5936de2b34b71407429",
+    "url": "/connect-four/static/media/win8.c24f65f1.wav"
+  }
+]);
